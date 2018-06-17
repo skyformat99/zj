e27845d53228c980fba72f5bfd309e2fa702233d
fanhui <hui.fan@mail.ru>
2018-06-18 02:32:26 +0800

    env tests: multi threads pass

--- src/ssh_cli.c
+++ src/ssh_cli.c
@@ -18,41 +18,41 @@ __init static void
 multi_threads_env_init(void) {
     //link with -lssh_threads
     //must be called before ssh_init()
-	//always return SSH_OK, need not check it's ret
+    //always return SSH_OK, need not check it's ret
     ssh_threads_set_callbacks(ssh_threads_get_pthread());
 
-	if(0 != ssh_init()){
-		__info("libssh multi_threads_env_init failed");
-		exit(1);
-	}
+    if(0 != ssh_init()){
+        __info("libssh multi_threads_env_init failed");
+        exit(1);
+    }
 }
 
 static void
 session_drop(ssh_session *session){
-	if(nil == *session){
-		return;
-	}
+    if(nil == *session){
+        return;
+    }
 
-	if(ssh_is_connected(*session)){
-		ssh_disconnect(*session);
-	}
+    if(ssh_is_connected(*session)){
+        ssh_disconnect(*session);
+    }
 
-	ssh_free(*session);
-	*session = nil;
+    ssh_free(*session);
+    *session = nil;
 }
 
 static void
 chan_drop(ssh_channel *chan){
-	if(nil == *chan){
-		return;
-	}
+    if(nil == *chan){
+        return;
+    }
 
-	if(!ssh_channel_is_closed(*chan)){
-		ssh_channel_close(*chan);
-	}
+    if(!ssh_channel_is_closed(*chan)){
+        ssh_channel_close(*chan);
+    }
 
-	ssh_channel_free(*chan);
-	*chan = nil;
+    ssh_channel_free(*chan);
+    *chan = nil;
 }
 
 //**exec once, and disconnect**
@@ -65,95 +65,93 @@ chan_drop(ssh_channel *chan){
 //@param cmd_info_siz[out]: 1 + strlen(cmd_info)
 static Error *
 exec_once(char *cmd, _i *exit_status, char **cmd_info, size_t *cmd_info_siz,
-		char *host, _i port, char *username, time_t conn_timeout_secs) {
-	__drop(session_drop) ssh_session session = ssh_new();
-	if(nil == session){
-		return __err_new(-1, "create ssh_session failed", nil);
-	}
+        char *host, _i port, char *username, time_t conn_timeout_secs) {
+    __drop(session_drop) ssh_session session = ssh_new();
+    if(nil == session){
+        return __err_new(-1, "create ssh_session failed", nil);
+    }
 
-	if(0 != ssh_options_set(session, SSH_OPTIONS_HOST, host)){
-		return __ssherr_new(session);
-	}
+    if(0 != ssh_options_set(session, SSH_OPTIONS_HOST, host)){
+        return __ssherr_new(session);
+    }
 
-	if(0 != ssh_options_set(session, SSH_OPTIONS_PORT, &port)){
-		return __ssherr_new(session);
-	}
+    if(0 != ssh_options_set(session, SSH_OPTIONS_PORT, &port)){
+        return __ssherr_new(session);
+    }
 
-	if(0 != ssh_options_set(session, SSH_OPTIONS_USER, username)){
-		return __ssherr_new(session);
-	}
+    if(0 != ssh_options_set(session, SSH_OPTIONS_USER, username)){
+        return __ssherr_new(session);
+    }
 
-	if(0 != ssh_options_set(session, SSH_OPTIONS_TIMEOUT, &conn_timeout_secs)){
-		return __ssherr_new(session);
-	}
+    if(0 != ssh_options_set(session, SSH_OPTIONS_TIMEOUT, &conn_timeout_secs)){
+        return __ssherr_new(session);
+    }
 
-	_i log_level = SSH_LOG_NOLOG;
-	if(0 != ssh_options_set(session, SSH_OPTIONS_LOG_VERBOSITY, &log_level)){
-		return __ssherr_new(session);
-	}
+    _i log_level = SSH_LOG_NOLOG;
+    if(0 != ssh_options_set(session, SSH_OPTIONS_LOG_VERBOSITY, &log_level)){
+        return __ssherr_new(session);
+    }
 
-	if(SSH_OK != ssh_connect(session)){
-		return __ssherr_new(session);
-	}
+    if(SSH_OK != ssh_connect(session)){
+        return __ssherr_new(session);
+    }
 
-	if(SSH_AUTH_SUCCESS != ssh_userauth_publickey_auto(session, nil, nil)){
-		return __ssherr_new(session);
-	}
+    if(SSH_AUTH_SUCCESS != ssh_userauth_publickey_auto(session, nil, nil)){
+        return __ssherr_new(session);
+    }
 
-	__drop(chan_drop) ssh_channel chan = ssh_channel_new(session);
-	if(nil == chan){
-		return __ssherr_new(session);
-	}
+    __drop(chan_drop) ssh_channel chan = ssh_channel_new(session);
+    if(nil == chan){
+        return __ssherr_new(session);
+    }
 
-	if(SSH_OK != ssh_channel_open_session(chan)){
-		return __ssherr_new(session);
-	}
+    if(SSH_OK != ssh_channel_open_session(chan)){
+        return __ssherr_new(session);
+    }
 
-	if(SSH_OK != ssh_channel_request_exec(chan, cmd)){
-		return __ssherr_new(session);
-	}
+    if(SSH_OK != ssh_channel_request_exec(chan, cmd)){
+        return __ssherr_new(session);
+    }
 
-	if(nil != exit_status){
-		*exit_status = ssh_channel_get_exit_status(chan);
-	}
+    if(nil != exit_status){
+        *exit_status = ssh_channel_get_exit_status(chan);
+    }
 
-	if(nil != cmd_info && nil != cmd_info_siz){
+    if(nil != cmd_info && nil != cmd_info_siz){
 #define __max_recv_siz 8 * 1024
-		*cmd_info_siz = 0;
-		*cmd_info = __malloc(__max_recv_siz);
-		*cmd_info_siz += sprintf(*cmd_info + *cmd_info_siz, "\n\x1b[31;01m"
-				"================\n"
-				"==== stderr ====\n"
-				"================\n"
-				"\x1b[00m");
-		size_t rcsiz = ssh_channel_read(chan, *cmd_info + *cmd_info_siz, __max_recv_siz - *cmd_info_siz, 1);
-		while(0 < rcsiz){
-			*cmd_info_siz += rcsiz;
-			rcsiz = ssh_channel_read(chan, *cmd_info + *cmd_info_siz, __max_recv_siz - *cmd_info_siz, 1);
-		}
+        *cmd_info_siz = 0;
+        *cmd_info = __malloc(__max_recv_siz);
+        *cmd_info_siz += sprintf(*cmd_info + *cmd_info_siz, "\n\x1b[31;01m"
+                "================\n"
+                "==== stderr ====\n"
+                "================\n"
+                "\x1b[00m");
+        size_t rcsiz = ssh_channel_read(chan, *cmd_info + *cmd_info_siz, __max_recv_siz - *cmd_info_siz, 1);
+        while(0 < rcsiz){
+            *cmd_info_siz += rcsiz;
+            rcsiz = ssh_channel_read(chan, *cmd_info + *cmd_info_siz, __max_recv_siz - *cmd_info_siz, 1);
+        }
 
-		*cmd_info_siz += sprintf(*cmd_info + *cmd_info_siz, "\n\x1b[31;01m"
-				"================\n"
-				"==== stdout ====\n"
-				"================\n"
-				"\x1b[00m");
-		rcsiz = ssh_channel_read(chan, *cmd_info + *cmd_info_siz, __max_recv_siz - *cmd_info_siz, 0);
-		while(0 < rcsiz){
-			*cmd_info_siz += rcsiz;
-			rcsiz = ssh_channel_read(chan, *cmd_info + *cmd_info_siz, __max_recv_siz - *cmd_info_siz, 0);
-		}
+        *cmd_info_siz += sprintf(*cmd_info + *cmd_info_siz, "\n\x1b[31;01m"
+                "================\n"
+                "==== stdout ====\n"
+                "================\n"
+                "\x1b[00m");
+        rcsiz = ssh_channel_read(chan, *cmd_info + *cmd_info_siz, __max_recv_siz - *cmd_info_siz, 0);
+        while(0 < rcsiz){
+            *cmd_info_siz += rcsiz;
+            rcsiz = ssh_channel_read(chan, *cmd_info + *cmd_info_siz, __max_recv_siz - *cmd_info_siz, 0);
+        }
 
-		++(*cmd_info_siz); //sizeof('\0')
+        ++(*cmd_info_siz); //sizeof('\0')
 #undef __max_recv_siz
-	}
+    }
 
-	ssh_channel_send_eof(chan);
-	return nil;
+    ssh_channel_send_eof(chan);
+    return nil;
 }
 
-//#define _UNIT_TEST
 #ifdef _UNIT_TEST
-#define _XOPEN_SOURCE 700
 #include "threadpool.c"
 #include "convey.c"
 
@@ -163,44 +161,48 @@ pthread_mutex_t mlock = PTHREAD_MUTEX_INITIALIZER;
 
 static void *
 thread_safe_checker(void *cnt){
-    exec_once("find /etc", nil, nil, nil, "localhost", 22, __UNIT_TEST_USERNAME, 3);
+    //async return, unless we waiting exit_status...
+    Error *e = exec_once("sleep 20", nil, nil, nil, "192.168.207.187", 22, "x", 10);
+    if(nil != e){
+        __display_errchain(e);
+    }
 
-	pthread_mutex_lock(&mlock);
-	if(__threads_total == ++(*((_i *) cnt))){
-		pthread_cond_signal(&cond);
-	}
-	pthread_mutex_unlock(&mlock);
+    pthread_mutex_lock(&mlock);
+    if(__threads_total == ++(*((_i *) cnt))){
+        pthread_cond_signal(&cond);
+    }
+    pthread_mutex_unlock(&mlock);
 
-	return nil;
+    return nil;
 }
 
 Main({
     Test("ssh cli tests", {
         Error *e = nil;
-		_i exit_status = 0;
-		char *recv_buf = nil;
-		size_t recv_siz = 0;
+        _i exit_status = 0;
+        char *recv_buf = nil;
+        size_t recv_siz = 0;
 
         Convey("thread safe", {
-			e = threadpool.init(__threads_total, __threads_total+1);
-			if(nil != e){
-				__display_errchain(e);
-				exit(1);
-			}
-			_i *cnt = __malloc(sizeof(_i));
-			*cnt = 0;
+            e = threadpool.init(__threads_total, __threads_total+1);
+            if(nil != e){
+                __display_errchain(e);
+                exit(1);
+            }
+            _i *cnt = __malloc(sizeof(_i));
+            *cnt = 0;
 
-			for(_i i = 0; i < __threads_total; ++i){
-				threadpool.add(thread_safe_checker, cnt);
-			}
+            for(_i i = 0; i < __threads_total; ++i){
+                threadpool.add(thread_safe_checker, cnt);
+            }
 
-			pthread_mutex_lock(&mlock);
-			while(__threads_total > *cnt){
-				pthread_cond_wait(&cond, &mlock);
-			}
-			pthread_mutex_unlock(&mlock);
+            pthread_mutex_lock(&mlock);
+            while(__threads_total > *cnt){
+                pthread_cond_wait(&cond, &mlock);
+            }
+            pthread_mutex_unlock(&mlock);
 
-			So(__threads_total == *cnt);
+            So(__threads_total == *cnt);
         });
 
         Convey("no output", {
@@ -210,9 +212,9 @@ Main({
             }
 
             So(nil == e);
-			So(0 == exit_status);
-			So(nil == recv_buf);
-			So(0 == recv_siz);
+            So(0 == exit_status);
+            So(nil == recv_buf);
+            So(0 == recv_siz);
         });
 
         Convey("no output, must fail", {
@@ -222,9 +224,9 @@ Main({
             }
 
             So(nil == e);
-			So(0 != exit_status);
-			So(nil == recv_buf);
-			So(0 == recv_siz);
+            So(0 != exit_status);
+            So(nil == recv_buf);
+            So(0 == recv_siz);
         });
 
         Convey("have output", {
@@ -234,11 +236,11 @@ Main({
             }
 
             So(nil == e);
-			So(0 == exit_status);
-			So(nil != recv_buf);
-			//__info(recv_buf);
-			//printf("%zd, %zd\n", 1 + strlen(recv_buf), recv_siz);
-			So((1 + strlen(recv_buf)) == recv_siz);
+            So(0 == exit_status);
+            So(nil != recv_buf);
+            //__info(recv_buf);
+            //printf("%zd, %zd\n", 1 + strlen(recv_buf), recv_siz);
+            So((1 + strlen(recv_buf)) == recv_siz);
         });
 
         Convey("have output, must fail", {
@@ -248,11 +250,11 @@ Main({
             }
 
             So(nil == e);
-			So(0 != exit_status);
-			So(nil != recv_buf);
-			//__info(recv_buf);
-			//printf("%zd, %zd\n", 1 + strlen(recv_buf), recv_siz);
-			So((1 + strlen(recv_buf)) == recv_siz);
+            So(0 != exit_status);
+            So(nil != recv_buf);
+            //__info(recv_buf);
+            //printf("%zd, %zd\n", 1 + strlen(recv_buf), recv_siz);
+            So((1 + strlen(recv_buf)) == recv_siz);
         });
     });
 })
