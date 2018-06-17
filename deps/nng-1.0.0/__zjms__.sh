bca03a76e3e21544e350869c9d3663ed970aa38d
fanhui <hui.fan@mail.ru>
2018-06-17 18:37:31 +0800

    USE nng's http_server

--- deps/nng-1.0.0/__zjms__.sh
+++ deps/nng-1.0.0/__zjms__.sh
@@ -6,45 +6,45 @@ rm -rf $path $buildpath 2>/dev/null
 
 mkdir __zjmsbuild__
 if [ 0 -ne $? ]; then
-	exit 1
+    exit 1
 fi
 
 cd __zjmsbuild__
 if [ 0 -ne $? ]; then
-	exit 1
+    exit 1
 fi
 
-cmake -G "Unix Makefiles" .. \
-	-DCMAKE_INSTALL_PREFIX=$path \
-	-DBUILD_SHARED_LIBS=OFF \
-	-DNNG_TESTS=OFF \
-	-DNNG_TOOLS=OFF \
-	-DNNG_ENABLE_TLS=OFF \
-	-DNNG_TRANSPORT_ZEROTIER=OFF \
-	-DNNG_ENABLE_HTTP=OFF \
-    -DNNG_TRANSPORT_WS=OFF \
+cmake -G "Unix Makefiles" ..\
+    -DCMAKE_INSTALL_PREFIX=$path\
+    -DBUILD_SHARED_LIBS=OFF\
+    -DNNG_TESTS=OFF\
+    -DNNG_TOOLS=OFF\
+    -DNNG_ENABLE_TLS=OFF\
+    -DNNG_TRANSPORT_ZEROTIER=OFF\
+    -DNNG_ENABLE_HTTP=ON\
+    -DNNG_TRANSPORT_WS=OFF\
     -DCMAKE_BUILD_TYPE=Release
 
 if [ 0 -ne $? ]; then
-	rm -rf $path $buildpath
-	exit 1
+    rm -rf $path $buildpath
+    exit 1
 fi
 
 njobs=`cat /proc/cpuinfo | grep 'processor' | wc -l`
 if [ 0 -eq $njobs ]; then
-	njobs=2
+    njobs=2
 fi
 
 make -j $njobs
 if [ 0 -ne $? ]; then
-	rm -rf $path $buildpath
-	exit 1
+    rm -rf $path $buildpath
+    exit 1
 fi
 
 make install/strip
 if [ 0 -ne $? ]; then
-	rm -rf $path $buildpath
-	exit 1
+    rm -rf $path $buildpath
+    exit 1
 fi
 
 make clean
